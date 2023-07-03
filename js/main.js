958f44a10114dcda625f824301af9797c3fce0d7
pagliucamauricio504 <pagliucamauricio@gmail.com>
2023-07-02 21:15:30 -0300

    Nueva rama PreEntrega2

--- js/main.js
+++ js/main.js
@@ -27,3 +27,45 @@ if (nombre != "" && apellido != "") {
 } else {
   alert("Hola desconocido");
 }
+
+
+// array
+var datos = [];
+
+var edadValida = false;
+var telefonoValido = false;
+
+//me costo (bastante) encontrarle la vuelta en esta condicion con el uso de operadores "!" y "||", y me ayude con el ChatGPT
+while (!edadValida || !telefonoValido) {
+  var edad = prompt("Ingrese su edad (en números):");
+  var telefono = prompt("Ingrese su número de teléfono (al menos ocho dígitos):");
+  var sugerencias = prompt("Ingrese sus sugerencias (opcional):");
+
+  
+  if (isNaN(edad)) {
+    alert("Error: La edad ingresada no es un número válido.");
+  } else {
+    edadValida = true;
+  }
+
+  //uso de metodo lengh
+  if (telefono.length < 8) {
+    alert("Error: El número de teléfono debe tener al menos 8 dígitos.");
+  } else {
+    telefonoValido = true;
+  }
+
+  //uso de metodo push para agregar datos al array - me volvi loco con el parentesis en el push
+  if (edadValida && telefonoValido) {
+      datos.push({
+      edad: parseInt(edad),
+      telefono: telefono,
+      sugerencias: sugerencias
+    });
+
+    
+    var mensaje = "Tus datos ingresados son: Edad: " + datos[0].edad + ", Teléfono: " + datos[0].telefono;
+
+    alert(mensaje);
+  }
+}
